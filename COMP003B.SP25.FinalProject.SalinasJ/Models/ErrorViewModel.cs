05eacd569de807298ca3868cc6611f1e41621e97
SalinasJason <jasonsalinas770@gmail.com>
2025-05-08 20:25:47 -0700

    Add project files.

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
