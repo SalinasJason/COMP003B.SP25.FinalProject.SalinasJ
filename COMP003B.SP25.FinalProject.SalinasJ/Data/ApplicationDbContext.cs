a64d9e47391dde0cad78d801bc206b53bd001a83
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 02:07:44 -0700

    Created ApplicationDbContext class that inherites from DbContext and configured DbSets for all entities

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using COMP003B.SP25.FinalProject.SalinasJ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.SalinasJ.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Organizer> Organizers { get; set; }
+        public DbSet<EventDetail> EventDetails { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<SpecialGuest> SpecialGuests { get; set; }
+    }
+}
