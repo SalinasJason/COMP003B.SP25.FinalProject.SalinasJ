b303abd4e0a15d7f7112ef94114fac61bb382ba1
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 01:36:54 -0700

    Added a model named SpecialGuest with properties

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/SpecialGuest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class SpecialGuest
+    {
+        public int SpecialGuestId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<EventDetail>? EventDetails { get; set; }
+    }
+}
