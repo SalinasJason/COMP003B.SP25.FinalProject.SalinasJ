3b5d6570fd86ed55d9625add054aeccd7332adad
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 01:57:03 -0700

    Added a model named EventDetail with properties that help manage relationships

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/EventDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class EventDetail
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int OrganizerId { get; set; }
+        public int VenueId { get; set; }  
+        public int SpecialGuestId { get; set; }  
+
+        public virtual Event? Events { get; set; }
+        public virtual Organizer? Organizer { get; set; }
+        public virtual Venue? Venue { get; set; }  
+        public virtual SpecialGuest? SpecialGuest { get; set; } 
+    }
+}
