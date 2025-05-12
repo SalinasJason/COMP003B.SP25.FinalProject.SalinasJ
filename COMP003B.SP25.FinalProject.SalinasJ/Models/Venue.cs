1a533db0161b4da7aeec205567d719e864400eca
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 00:43:50 -0700

    Added a model named Venue with properties

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/Venue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Owner { get; set; }
+
+        [Required]
+        [Phone]
+        public string OwnerPhoneNumber { get; set; }
+
+        public virtual ICollection<EventDetail>? EventDetails { get; set; }
+    }
+}
