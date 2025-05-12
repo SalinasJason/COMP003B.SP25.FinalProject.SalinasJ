d58aad9f7105ec192971029afd2051309cee030f
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 00:25:46 -0700

    Added a model named Organizer with properties

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/Organizer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class Organizer
+    {
+        public int OrganizerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Role { get; set; }
+
+        public virtual ICollection<EventDetail>? EventDetails { get; set; }
+    }
+}
