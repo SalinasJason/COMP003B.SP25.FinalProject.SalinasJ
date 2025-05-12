13982fb71b982e52c0927b4c2f5eb7c87d566d3c
SalinasJason <jasonsalinas770@gmail.com>
2025-05-12 00:10:20 -0700

    Added a model named Event with properties

---
+++ COMP003B.SP25.FinalProject.SalinasJ/Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.SalinasJ.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Date { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Time { get; set; }
+
+        public virtual ICollection<EventDetail>? EventDetails { get; set; }
+    }
+}
